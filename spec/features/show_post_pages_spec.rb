13352c28286c27236701d76a7ec50a8b33582536
Duane M. Moody <dmmoody@gmail.com>
2015-03-20 11:35:25 -0700

    add feature specs for post#new

--- spec/features/show_post_pages_spec.rb
+++ spec/features/show_post_pages_spec.rb
@@ -21,4 +21,36 @@ describe 'the show post process' do
     click_on "Return to blog list"
     expect(page).to have_content("Hello World")
   end
+
+  it 'adds a new post' do
+    visit posts_path
+    click_on "Add a new post"
+    fill_in "Title", :with => "Hello World"
+    fill_in "Entry", :with => "My first entry"
+    click_on "Create Post"
+    expect(page).to have_content('Hello World')
+  end
+
+  it 'shows errors when title and entry is not filled in' do
+    visit posts_path
+    click_on "Add a new post"
+    click_on "Create Post"
+    expect(page).to have_content('errors')
+  end
+
+  it 'shows errors when title is not filled in' do
+    visit posts_path
+    click_on "Add a new post"
+    fill_in "Entry", :with => "My first entry"
+    click_on "Create Post"
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  it 'shows errors when entry is not filled in' do
+    visit posts_path
+    click_on "Add a new post"
+    fill_in "Title", :with => "Hello World"
+    click_on "Create Post"
+    expect(page).to have_content("Entry can't be blank")
+  end
 end
