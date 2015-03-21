7316e17447ef6ac85eb327ada1a80e20c9cda102
Duane M. Moody <dmmoody@gmail.com>
2015-03-21 14:22:09 -0700

    add post_id column and comment specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
   validates :author, :presence => true
   validates :entry, :presence => true
 end
---
+++ db/migrate/20150320194303_add_column_post_id.rb
@@ -0,0 +1,5 @@
+class AddColumnPostId < ActiveRecord::Migration
+  def change
+    add_column :comments, :post_id, :int
+  end
+end
